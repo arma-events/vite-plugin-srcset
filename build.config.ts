1c66b545bca931e08e5351a0b7ea8ca749967b4d
DerZade <DerZade@gmail.com>
2024-11-05 20:16:03 +0100

    chore: :wrench: disable rollup's named & default exports together warning

--- build.config.ts
+++ build.config.ts
@@ -6,6 +6,9 @@ export default defineBuildConfig({
     externals: ['vite'],
     declaration: true,
     rollup: {
-        emitCJS: true
+        emitCJS: true,
+        output: {
+            exports: 'named'
+        }
     }
 });
